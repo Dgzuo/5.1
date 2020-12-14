541ef2b0663f57458ca33071a64fdb8f8442e5f3
Вова Судаков <shopvitiaz@rambler.ru>
2020-12-14 14:25:36 +0200

    Добавьте файлы проекта.

---
+++ 5.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+namespace _5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int n = r.Next(5, 30);
+            int[] arr = new int[n];
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(1, 200);
+            }
+
+            string result = string.Join(", ", arr);
+
+            Console.WriteLine(result);
+            Console.WriteLine("Min = " + Enumerable.Min(arr));
+            Console.WriteLine("Max = " + Enumerable.Max(arr));
+        }
+    }
+}
